70cd02b143c7d8d7b2b8c92c5a11479820f2d612
Syahmi Rahim <syahmi_98@hotmail.com>
2021-11-17 09:52:51 +0800

    express web app created to be configured

--- graph-tutorial/app.js
+++ graph-tutorial/app.js
@@ -13,6 +13,41 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
+// Session middleware
+// NOTE: Uses default in-memory session store, which is not
+// suitable for production
+app.use(session({
+  secret: 'your_secret_value_here',
+  resave: false,
+  saveUninitialized: false,
+  unset: 'destroy'
+}));
+
+// Flash middleware
+app.use(flash());
+
+// Set up local vars for template layout
+app.use(function(req, res, next) {
+  // Read any flashed errors and save
+  // in the response locals
+  res.locals.error = req.flash('error_msg');
+
+  // Check for simple error string and
+  // convert to layout's expected format
+  var errs = req.flash('error');
+  for (var i in errs){
+    res.locals.error.push({message: 'An error occurred', debug: errs[i]});
+  }
+
+  // Check for an authenticated user and load
+  // into response locals
+  if (req.session.userId) {
+    res.locals.user = app.locals.users[req.session.userId];
+  }
+
+  next();
+});
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
--- graph-tutorial/routes/index.js
+++ graph-tutorial/routes/index.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  let params = {
+    active: { home: true }
+  };
+
+  res.render('index', params);
 });
 
 module.exports = router;
